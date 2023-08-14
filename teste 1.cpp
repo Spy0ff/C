d7e3e83cb2dfed984051510b99a059f38ca01179
Spy0ff <141172366+Spy0ff@users.noreply.github.com>
2023-08-14 10:27:11 -0300

    Add files via upload
    program that takes two numbers and shows which one is bigger
    programa que recebe dois numeros e mostra qual o maior

---
+++ teste 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+	
+int main(){
+	int n1,n2;
+	printf("Digite o primeiro numero\n");
+	scanf("%d",&n1);
+	printf("Digite o segundo numero\n");
+	scanf("%d",&n2);
+	
+	if(n1>n2){
+	printf("O numero %d e maior",n1);
+	}else{
+		printf("O numero %d e maior",n2);
+	}
+}
